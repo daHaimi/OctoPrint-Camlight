0ee71b89534315c3f8681b5a5762832dd292971c
Mathias Haimerl <mathias.haimerl@gmail.com>
2016-04-10 01:03:43 +0200

    update hell

--- octoprint_camlight/static/js/camlight.js
+++ octoprint_camlight/static/js/camlight.js
@@ -38,7 +38,8 @@ $(function() {
                         }
                     }
                 }
-            });            
+            });  
+            self.ledCmd();          
         });
 
 
